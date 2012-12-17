758f46b2a1570d6706b47829ed41703a3a83c252
Vasii Cosmin-Radu <cosminradu.vasii@gmail.com>
2012-12-17 19:40:44 +0200

    reparam

--- DocumentProcessingWebManager/DocumentProcessingWebManager/src/main/java/com/mps/batmanii/ocrWebManager/beans/XmlFile.java
+++
@@ -1,8 +0,0 @@
-package com.mps.batmanii.ocrWebManager.beans;
-
-public class XmlFile {
-
-	private String execType;
-	private String execName;
-	private String rootElement;
-}
--- DocumentProcessingWebManager/DocumentProcessingWebManager/src/main/webapp/resources/js/cufon-replace.js
+++
@@ -1,1 +0,0 @@
-Cufon.replace('h7, h2, h1 a', { fontFamily: 'Dynalight', hover:true });
